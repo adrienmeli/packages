590a7d31df30ee3d02a8e2ae43f4e14bd58a9d24
adrienmeli <adrienmeli@gmail.com>
2020-11-27 17:30:30 +0100

    automatic commit

--- list-packages.R
+++ list-packages.R
@@ -1,4 +1,3 @@
 ip=installed.packages()
 ip=as.data.frame(ip[,1])
 write.table(ip, "list-of-installed-packages.csv", row.names = F, col.names = F, quote = T)
-as.vector(read.table("list-of-installed-packages.csv", quote = ""))
